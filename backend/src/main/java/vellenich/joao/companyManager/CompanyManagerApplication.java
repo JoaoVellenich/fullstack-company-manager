5c7465ff5c2bb693da10440bfa6c524ac9a383c0
JoaoVellenich <jpvellenich@gmail.com>
2026-02-24 17:49:48 -0300

    feat: start backend project

---
+++ backend/src/main/java/vellenich/joao/companyManager/CompanyManagerApplication.java
@@ -0,0 +1,13 @@
+package vellenich.joao.companyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/vellenich/joao/companyManager/CompanyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package vellenich.joao.companyManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
